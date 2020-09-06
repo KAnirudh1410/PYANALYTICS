1930fe8600f89e618021c615c155d217d5657dc1
K.Anirudh1410 <um20193@stu.ximb.ac.in>
2020-09-06 13:55:04 +0530

    Create XIM1.py
    Session 1- 6 sept

---
+++ XIM1.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 20:14:48 2020
+
+@author: LENEVO
+"""
+
+print?
+print ("Anirudh")
+print("Anirudh","Legendary")
+print("Anirudh","Legendary", sep='***',end='***')
+help(print)
+
+help('OPERATORS')
+
+    A=10
+    print(A)
+!pip install pandas
+    !python --version
+!pip list
+b=input("your country")
+
+a=20.22
+Num=33
+type(b)
+Num=int(Number)
+type(Num)
+
+country="India"
+print("I live in {0}" which is very good country.format(Country))
+countrycode=91
+
+print("i live in {1} which is ery good country".format(country,countrycode))
+
+a=1.11
+b=int(a)
+c=float(b)
+
+x=2
+y=3
+z=x+y
+print(z)
+print(x)
+
+z
+print(x-y)
+print(x/y)
+x=x-1
+x=6
+
+y=x**3
+x**=2
+x
+y
+y-=1
+y
+
+a=10
+b=11
+
+#Modulo 
+b%3
+a%3
+
+'''
+AND
+A B X
+0 0 0
+1 1 1
+1 0 0
+0 1 0
+
+'''
+A=0
+B=1
+
+X= A and B
+
+A=True
+B=False
+X= A or B
+
+X
+
+type(X)
+'''
+Not
+A X
+0 1
+1 0
+'''
+
+A=0
+X= not(A)
+X
+
+A=1
+X= not(A)
+X
+
+A=15
+bool(A)
+
+B=0
+bool(B)
+
+Fname="Anirudh"
+Lname="K"
+
+name= Fname + "  "+ Lname
+name
+
+
+len(Fname)
+
+Fname.capitalize()
+Fname.upper()
+
+Fname=Fname.rjust(10)
+print(Fname)
+
+name=name.replace('111' , '133')
+name
